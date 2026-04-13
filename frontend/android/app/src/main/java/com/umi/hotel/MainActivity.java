b66bf69072b9c2b00ea19cb6b9aca6e8c3f7cf88
Nattapong Suwan <nssuwan186-dev@github.com>
2026-04-14 00:26:03 +0700

    Fix AndroidManifest and MainActivity

--- frontend/android/app/src/main/java/com/umi/hotel/MainActivity.java
+++ frontend/android/app/src/main/java/com/umi/hotel/MainActivity.java
@@ -1,6 +1,41 @@
 package com.umi.hotel;
 
 import android.os.Bundle;
-import com.getcapacitor.BridgeActivity;
+import androidx.appcompat.app.AppCompatActivity;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.webkit.WebSettings;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+        WebView webView = new WebView(this);
+        WebSettings settings = webView.getSettings();
+        settings.setJavaScriptEnabled(true);
+        settings.setDomStorageEnabled(true);
+        settings.setDatabaseEnabled(true);
+        
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                view.loadUrl(url);
+                return true;
+            }
+        });
+        
+        webView.loadUrl("file:///android_asset/index.html");
+        setContentView(webView);
+    }
+    
+    @Override
+    public void onBackPressed() {
+        WebView webView = (WebView) findViewById(android.R.id.content);
+        if (webView != null && webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
